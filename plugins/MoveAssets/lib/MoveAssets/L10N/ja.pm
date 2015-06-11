614b6ef5be176dd6e20ffc7fc0472fa8cebfbf69
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2015-06-11 10:03:24 +0000

    L10N 追加。

---
+++ plugins/MoveAssets/lib/MoveAssets/L10N/ja.pm
@@ -0,0 +1,12 @@
+package MoveAssets::L10N::ja;
+use strict;
+use warnings;
+
+use base qw( MoveAssets::L10N );
+
+our %Lexicon
+    = ( 'Move assets when changing site path.' =>
+        'サイトパス／アーカイブパスを変更した時に、アイテムのパスも変更します。',
+    );
+
+1;
